be2335743be716d55bea8233de9f0a9cb2f84f5b
ho3innn <ho3in.teimouri@gmail.com>
2020-09-30 20:02:59 +0330

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public abstract class Character :MonoBehaviour
+{
+   
+    
+    private int height;
+    private int weight;
+    private int Gender;
+    private int stamina;
+    private string characterName="";
+    private float moveSpeed;
+
+    public abstract void UseStamina(int stamina);
+    public abstract void Movement();
+}
